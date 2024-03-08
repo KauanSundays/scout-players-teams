d9daa24e4b9cd04832e6f7367ddb6805b530c347
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-03-07 21:48:23 -0300

    tabela users customizada

--- agencia-carreiras-faculdades/database/migrations/2014_10_12_000000_create_users_table.php
+++ agencia-carreiras-faculdades/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('tipo_usuario', ['faculdade', 'avaliador']);
+            $table->string('instituicao_nome')->nullable(); // caso for faculdade
+            $table->string('avaliador_nome')->nullable(); // caso for avaliador
+            $table->string('localizacao')->nullable();
             $table->timestamps();
         });
     }
